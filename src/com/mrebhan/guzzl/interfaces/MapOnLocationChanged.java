b045c77c2a5e86872845bf0e3a59debf2d0014f9
markrebhan <mark.rebhan0@gmail.com>
2014-01-20 14:39:54 -0600

    Delete MapOnLocationChanged.java

--- src/com/mrebhan/guzzl/interfaces/MapOnLocationChanged.java
+++
@@ -1,8 +0,0 @@
-package com.mrebhan.guzzl.interfaces;
-
-/* 
- * This interface is used as a callback from the Location service to relay position back to activity
- */
-public interface MapOnLocationChanged {
-	public void mapOnLocationChanged(double lat, double lon);
-}
